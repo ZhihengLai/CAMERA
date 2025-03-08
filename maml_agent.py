755f12ced568e43db7a476867958de5d7342ae0e
ZHLai <113399268+ZhihengLai@users.noreply.github.com>
2025-03-08 22:47:40 +0800

    Add files via upload

--- maml_agent.py
+++ maml_agent.py
@@ -3,65 +3,41 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
+from torch.cuda.amp import GradScaler
+from tqdm import tqdm
+
 import numpy as np
 import time
 import learn2learn as l2l
-from networks.FT_net.fourier_net import FourierNet  # Assuming OFENet is implemented and imported from another module
-from networks.policy_net.PPO import GaussianActor, CriticV, PPO, RolloutBuffer  # PPO components
+from policy.PPO import GaussianActor, CriticV, PPO, RolloutBuffer  # PPO components
 from blocks.maml_config import Metaworld
 
-
-class New_maml_agent(nn.Module):
+class MAML_agent(nn.Module):
     def __init__(self, env, agent_config, maml_config):
-        super(New_maml_agent, self).__init__()
+        super(MAML_agent, self).__init__()
         self.env = env
-        self.eval_env = env #将评估环境设置为与训练环境一样
+        self.eval_env = env
 
         self.args = agent_config
-        #从环境中提取状态和动作空间的维度
+
         state_dim = env.observation_space.shape[0]
         action_dim = env.action_space.shape[0]
         max_action = float(env.action_space.high[0])
-        #初始化回放缓存区
-        # TODO: discount and lam?
-        self.replay_buffer = RolloutBuffer(obs_dim=state_dim, act_dim=action_dim, capacity=agent_config.steps_per_epoch, gamma=agent_config.discount, lam=agent_config.lam)
-        
-        #设置特征提取器的参数
-        self.extractor_kwargs = {
-            "dim_state": state_dim, 
-            "dim_action": action_dim, 
-            "dim_output": 11,
-            "dim_discretize": agent_config.dim_discretize, 
-            "total_units": 240,
-            "num_layers": 6,
-            "batchnorm": True,
-            "fourier_type": agent_config.fourier_type, 
-            "discount": agent_config.discount, 
-            "projection_dim": agent_config.projection_dim, 
-            "cosine_similarity": agent_config.cosine_similarity,
-            "normalizer": agent_config.normalizer
-        }
 
-        #Initialize the extractor and target extractor network
-        self.extractor = FourierNet(**self.extractor_kwargs, skip_action_branch=False)
-        self.target_extractor = FourierNet(**self.extractor_kwargs, skip_action_branch = False)
-        self.soft_update(self.target_extractor, self.extractor, tau=1)
+        self.replay_buffer = RolloutBuffer(obs_dim=state_dim, act_dim=action_dim, capacity=agent_config.steps_per_epoch, gamma=agent_config.discount, lam=agent_config.lam)
 
         self.actor = GaussianActor(
-            self.extractor.dim_state_features,
-            action_dim, max_action, layer_units=(64, 64))
+            state_dim, action_dim, max_action, layer_units=(64, 64))
         self.critic = CriticV(
-            self.extractor.dim_state_features, units=(64, 64))
+            state_dim, units=(64, 64))
         
-        self.critic = l2l.algorithms.MAML(self.critic, lr = 1e-3, first_order=False)
+        self.critic = l2l.algorithms.MAML(self.critic, lr = 1e-3)
 
-        #初始化PPO策略网络
         self.policy = PPO(
-            feature_extractor = self.extractor,
             actor=self.actor,
             critic=self.critic,
-            pi_lr=3e-4, # adapt learning rate for actor
-            vf_lr=1e-3, # adapt learning rate for critic
+            pi_lr=3e-4,
+            vf_lr=1e-3,
             clip_ratio=0.2,
             batch_size=64,
             discount=0.99,
@@ -69,31 +45,30 @@ class New_maml_agent(nn.Module):
             horizon=2048,
             gpu=0)
         
-    #更新目标网络的参数，tau=1时为复制
-    def soft_update(self, target, source, tau = 1):
-        for target_param, source_param in zip(target.parameters(), source.parameters()):
-            target_param.data.copy_((1 - tau) * target_param.data + tau * source_param.data)
+    def soft_update(self, target, source, tau=1):
+        for target_param, source_param in zip(target.state_dict().values(), source.state_dict().values()):
+            if isinstance(target_param, torch.Tensor) and isinstance(source_param, torch.Tensor):
+                target_param.copy_((1 - tau) * target_param + tau * source_param)
 
-    def get_action(self, x, action=None ,return_distribution=False):
-        state_features = self.extractor.features_from_states(x)
-
+    def get_action(self, state, action=None ,return_distribution=False):
         # Get the action mean from the PPO actor
-        action_mean, action_logstd = self.actor(state_features)
+        action_mean, action_logstd = self.actor(state) #得到了动作的均值和对数标准差
         action_std = torch.exp(action_logstd)
         distribution = Normal(action_mean, action_std)
 
         # Sample action or return log probability
+        #如果有给定action参数，那么从刚刚计算的分布中采样一个动作并计算该动作的对数概率
         if action is None:
             action = distribution.sample()
             logprob = distribution.log_prob(action).sum(1)
         else:
             logprob = distribution.log_prob(action).sum(1)
 
+        ##区别就是是否多返回一个动作的概率分布
         if not return_distribution:
             return action, logprob, distribution.entropy().sum(1)
         else:
             return action, logprob, distribution.entropy().sum(1), distribution
-
 
     def adapt(self, num_steps, information, config:Metaworld, lifetime_buffer, mean_reward_for_baseline, device):
 
@@ -103,58 +78,18 @@ class New_maml_agent(nn.Module):
         total_steps = self.args.steps_per_epoch*epochs
         steps_per_epoch = self.args.steps_per_epoch
 
-        batch_size = self.args.batch_size 
-
+        #重置环境，初始化回合的总奖励和时间步数
         state, _ = self.env.reset()
-
         episode_return = 0
         episode_timesteps = 0
 
-        print("Initialization: I am collecting samples randomly!")
-        round = 0
-        for i in range(self.args.random_collect):
-            print("Collect Round: ", round)
-            round += 1
-            action = self.env.action_space.sample()
-            next_state, reward, terminated, truncated, _ = self.env.step(action)
-            done = terminated or truncated
-
-            episode_return += reward
-            episode_timesteps += 1
-
-            done_flag = done
-            if episode_timesteps == config.max_episode_steps:
-                done_flag = False
-
-            self.replay_buffer.add(obs=state, act=action, obs2=next_state, rew=reward, done=done_flag, val=0, logp=0)
-            state = next_state
-
-            if done:
-                state, _ = self.env.reset()
-                episode_timesteps = 0
-                episode_return = 0
-
-        self.policy.clone_for_adaptation() # clone extractor and critic network
+        # No need to pretrain the extractor!
+        self.policy.clone_for_adaptation()
         self.policy.set_optimizers()
-
-        print("Pretrain: I am pretraining the extractor!")
-        pretrain_step = 0
-        for i in range(self.args.pre_train_step): 
-            print("Pretrain Step: ", pretrain_step)
-            pretrain_step += 1
-            sample_states, sample_actions, sample_next_states, _, sample_dones = self.replay_buffer.sample(batch_size=self.args.batch_size)
-            sample_next_actions, _ = self.policy.get_action(sample_next_states)
-
-            #adapt extractor
-            pred_loss = self.policy.extractor_loss_for_adaptation(self.extractor, self.target_extractor, sample_states, sample_actions, sample_next_states, sample_next_actions, sample_dones)
-            
-            self.policy.extractor_adapt(pred_loss)
-
 
         state = np.array(state, dtype = np.float32)
 
-        self.replay_buffer.get()
-
+        #生命周期缓冲区初始化
         done_lifetime = information['prev_done']
         episodes_returns_lifetime=[]
         episodes_successes_lifetime=[]
@@ -164,30 +99,34 @@ class New_maml_agent(nn.Module):
 
         print("Train: I am starting to train myself!")
         step = 0
-        for cur_steps in range(total_steps):
-            print("Train Step: ", step)
+
+        #初始化actor混合精度训练所需的gradscaler
+        actor_scaler = GradScaler()
+
+        for cur_steps in tqdm(range(total_steps)):
             prev_done_lifetime = done_lifetime
 
             step += 1
-            action, logp, v_t = self.policy.get_action_and_val(state)
+            action, logp, v_t = self.policy.get_action_and_val(state) #从当前策略获得动作  动作概率  状态值估计
 
             # Step the env
-            next_state, reward, terminated, truncated, info = self.env.step(action)
+            next_state, reward, terminated, truncated, info = self.env.step(action) #在环境中执行这个动作
             done = terminated or truncated
-            done_lifetime = torch.Tensor(done, dtype=torch.float32)
+            #组合这里torch,Tensor不支持布尔型变量和float组合
+            done_lifetime = torch.tensor(done, dtype=torch.float32)
             episode_timesteps += 1
             episode_return += reward
 
             done_flag = done
 
-            # # done is valid, when an episode is not finished by max_step.
-            # if episode_timesteps == config.max_episode_steps:
-            #     done_flag = False
-
             self.replay_buffer.add(obs=state, act=action, obs2=next_state, rew=reward, done=done_flag, val=v_t, logp=logp)
 
-            lifetime_buffer.store_step_data(global_step=current_lifetime_step, obs=state.to(device), act=action.to(device),
-                                            reward=reward.to(device), logp=logp.to(device), prev_done=prev_done_lifetime.to(device))
+            lifetime_buffer.store_step_data(global_step=current_lifetime_step,
+                                            obs=torch.tensor(state, dtype=torch.float32).to(device),
+                                            act=torch.tensor(action, dtype=torch.float32).to(device),
+                                            reward=torch.tensor(reward, dtype=torch.float32).to(device),
+                                            logp=torch.tensor(logp, dtype=torch.float32).to(device),
+                                            prev_done=torch.tensor(prev_done_lifetime, dtype=torch.float32).to(device))
 
             state = next_state
             current_lifetime_step += 1
@@ -210,29 +149,10 @@ class New_maml_agent(nn.Module):
                 else:
                     episodes_successes_lifetime.append(0.0)
 
-
-            update_every = self.args.update_every 
-            #Update the extractor every update_every steps
-            if self.args.gin is not None and cur_steps % update_every == 0: 
-                for _ in range(update_every):
-                    sample_states, sample_actions, sample_next_states, _, sample_dones = self.replay_buffer.sample(
-                        batch_size=batch_size)
-                    sample_next_actions, _ = self.policy.get_action(sample_next_states)
-
-                    #adapt extractor
-                    pred_loss = self.policy.extractor_loss_for_adaptation(self.target_extractor, sample_states, sample_actions, sample_next_states, sample_next_actions, sample_dones)
-                    self.policy.extractor_adapt(pred_loss)
-
-            #Update the target network every target_update_freq steps
-            if self.args.gin is not None and cur_steps % self.args.target_update_freq == 0: 
-                self.soft_update(self.target_extractor, self.extractor, self.args.tau) 
-
-
             if (cur_steps + 1) % steps_per_epoch == 0: #Train the policy every steps_per_epoch steps
-                self.policy.train(self.replay_buffer)
-                
+                self.policy.train(self.replay_buffer, actor_scaler)            
 
-
+        ##将本次任务适配过程中收集的奖励、成功率数据存入lifetime_buffer
         lifetime_buffer.episodes_returns=lifetime_buffer.episodes_returns+episodes_returns_lifetime
         lifetime_buffer.episodes_successes =lifetime_buffer.episodes_successes+ episodes_successes_lifetime
 
@@ -240,28 +160,32 @@ class New_maml_agent(nn.Module):
 
         information={'prev_done': done_lifetime, 'current_lifetime_step': current_lifetime_step,
                      'current_episode_return': episode_return_lifetime, 'current_episode_success': succeeded_in_episode_lifetime}
-        
-        return information
 
-    def evaluate_extractor_and_critic(self, config:Metaworld):
+        return information
+    
+    def evaluate_critic(self, config:Metaworld):
         total_eval_steps = config.num_env_steps_for_estimating_maml_loss
-        batch_size = self.args.batch_size
 
         state, _ = self.eval_env.reset() 
         step = 0
-        for cur_steps in range(total_eval_steps):
-            print(f"Eval Step: {step}")  
+        episode_timesteps = 0
+        episode_return = 0
+        
+        print("Evaluation Begins.")
+        for cur_steps in tqdm(range(total_eval_steps)):
             step += 1
+            #使用当前状态从策略网络获得该执行的动作和对数概率和状态价值
             action, logp, v_t = self.policy.get_action_and_val(state)
 
             # Step the env
             next_state, reward, terminated, truncated, _ = self.env.step(action)
             done = terminated or truncated
-            episode_timesteps += 1
-            episode_return += reward
+            episode_timesteps += 1 #当前回合的时间步加1
+            episode_return += reward #当前回合的总奖励加1
 
             done_flag = done
 
+            #将这一回合的信息放在回放缓存区，更新状态
             self.replay_buffer.add(obs=state, act=action, obs2=next_state, rew=reward, done=done_flag, val=v_t, logp=logp)
             state = next_state
 
@@ -273,16 +197,12 @@ class New_maml_agent(nn.Module):
                 episode_timesteps = 0
                 episode_return = 0
 
-        evaluate_every = self.args.evaluate_every # TODO: 添加参数 evaluate_every
-        for _ in range(evaluate_every):
-            sample_states, sample_actions, sample_next_states, _, sample_dones = self.replay_buffer.sample(
-                batch_size=batch_size)
-            sample_next_actions, _ = self.policy.get_action(sample_next_states)
+        raw_states, actions, advantages, returns, logp_olds = self.replay_buffer.get()
+        critic_loss = self.policy.critic_loss_for_adaptation(raw_states, returns)
 
-            #backward propagation to the loss
-            pred_loss = self.policy.extractor_loss_for_adaptation(self.target_extractor, sample_states, sample_actions, sample_next_states, sample_next_actions, sample_dones)
-            pred_loss.backward()
+        critic_loss.backward() #报错了
+        print("大成功！！！！")
 
+        #重置克隆网络
         self.policy.reset_cloned_networks()
-
     
